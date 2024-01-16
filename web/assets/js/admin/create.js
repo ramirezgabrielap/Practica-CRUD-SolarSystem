29804874661263277e9e678259652f8e86768621
Gabriela Ramírez <ramirezgabrielap@gmail.com>
2024-01-16 19:20:44 -0300

    Cambio de idioma en el Front

--- web/assets/js/admin/create.js
+++ web/assets/js/admin/create.js
@@ -4,43 +4,52 @@ console.log("admin/create.js");
 document.addEventListener("DOMContentLoaded", async function () {
     console.log("admin.js ready")
 
+    const toastLiveExample = document.getElementById('liveToast');
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+
     document.getElementById("form").addEventListener('submit', create);
 
     function create(evt) {
-        let myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
+        evt.preventDefault();
+
+        console.log('create', evt);
 
         let name = document.getElementById("name").value
         let radio = document.getElementById("radio").value
         let mass = document.getElementById("mass").value
-        console.log(name);
-        let raw = JSON.stringify({
-            name: name,
-            radio: radio,
-            mass: mass
-        });
 
-        let requestOptions = {
+        fetch('http://localhost:8000/api/planets', {
             method: 'POST',
-            headers: myHeaders,
-            body: raw,
-            redirect: 'follow'
-        };
-
-        fetch("http://localhost:8000/api/planets", requestOptions)
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                name: name,
+                radio: radio,
+                mass: mass
+            })
+        })
             .then(response => {
+                console.log("2");
+                //console.log(response);
                 if (!response.ok) {
                     throw new Error(`HTTP error! Status: ${response.status}`);
                 }
-                return response.text();
+                return response.json();
             })
 
-            .then(result => {
+            .then(result => {//ok
+                console.log("3");
                 console.log(result);
-                window.location.href = "/admin/index.html";
+                // window.location.href = "/admin/index.html";
             })
 
-            .catch(error => console.log('error', error));
+            .catch(error => {
+                console.log("4");
+                console.log('error', error)
+
+                toastBootstrap.show()
+
+
+            });
     }
 
 });
