e3e669456c524c1e580c0fe81feaa17eea725f77
Gabriela Ramírez <ramirezgabrielap@gmail.com>
2024-01-18 01:25:50 -0300

    Cambios para pruebas

--- api/src/main/java/com/example/restservice/repository/PlanetRepository.java
+++ api/src/main/java/com/example/restservice/repository/PlanetRepository.java
@@ -3,7 +3,6 @@ package com.example.restservice.repository;
 import com.example.restservice.model.Planet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface PlanetRepository extends JpaRepository<Planet, Integer> {
 
 }
